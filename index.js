d3e7f4adfa0b2d69606640358e2d2d49e97006c6
rovenelabanga <rovenelabanga2001@gmail.com>
2024-09-27 13:04:06 +0300

    uses functions to manipulate objects

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee = {
+  name: "Katana",
+  streetAdress: "11 Broadway",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newObj = { ...employee };
+  newObj[key] = value;
+  return newObj;
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = function (
+  employee,
+  key,
+  value
+) {
+  employee[key] = value;
+  return employee;
+};
+
+const deleteFromEmployeeByKey = (employee, key) => {
+  const newObj = { ...employee };
+  delete newObj[key];
+  return newObj;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+console.log(destructivelyDeleteFromEmployeeByKey(employee, "name"));
+console.log(employee);
